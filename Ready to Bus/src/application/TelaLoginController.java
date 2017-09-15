8650b654385c82065d004c19005e817987f0bb17
João Victor Scanagatta <joaoscanagatta1@hotmail.com>
2017-09-14 22:10:33 -0300

    inicio tela login

---
+++ Ready to Bus/src/application/TelaLoginController.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class TelaLoginController {
+
+    @FXML
+    private TextField tfLogin;
+
+    @FXML
+    private Button btnCadastrar;
+
+    @FXML
+    private Button btnEntrar;
+
+    @FXML
+    private Button btnSobre;
+
+    @FXML
+    private Button btnWeb;
+
+    @FXML
+    private PasswordField pfSenha;
+
+}
