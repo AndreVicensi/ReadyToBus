6271d34156200c9af26a2a7d8c4fb6c4c9609ec7
João Victor Scanagatta <joaoscanagatta1@hotmail.com>
2017-09-17 13:26:53 -0300

    primeira tela aparecendo

--- Ready to Bus/src/visual/Main.java
+++ Ready to Bus/src/visual/Main.java
@@ -3,25 +3,42 @@ package visual;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	public static BorderPane root;
+
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("TelaInicial.fxml"));
-			Scene scene = new Scene(root,400,400);
+			root = (BorderPane) FXMLLoader.load(getClass().getResource("TelaInicial.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("TelaInicialStyle.css").toExternalForm());
 			primaryStage.setScene(scene);
+
+			primaryStage.setMaximized(true);
+
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("TelaLogin.fxml"));
+
+			AnchorPane loginView = (AnchorPane) loader.load();
+			root.setCenter(loginView);
+
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	public static BorderPane getRoot() {
+		return root;
+	}
+
 }
