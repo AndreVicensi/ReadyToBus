8a1070ee40704053c7777bfacac72100b4c31969
ander <ander@172.18.23.200>
2017-09-21 20:27:43 -0300

    conexao com o banco

---
+++ Ready to Bus/src/conexao/ConexaoProducao.java
@@ -0,0 +1,45 @@
+package conexao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoProducao  implements Conexao{
+
+	private static Connection con;
+
+	static {
+		open();
+	}
+	
+	public static void open() {
+		String url = "jdbc:mysql://localhost:3306/transporte";
+		String username = "root";
+		String password = "kr666";
+		try {
+			con = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public Connection get() {
+		try {
+			if(con.isClosed()) {
+				open();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	@Override
+	public void close() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Ready to Bus/src/conexao/ConexaoTeste.java
@@ -0,0 +1,47 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoTeste implements Conexao {
+	
+	private static Connection con;
+
+	static {
+		open();
+	}
+	
+	public static void open() {
+		String url = "jdbc:mysql://localhost:3306/transporte_test";
+		String username = "root";
+		String password = "kr666";
+		try {
+			con = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public Connection get() {
+		try {
+			if(con.isClosed()) {
+				open();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	@Override
+	public void close() {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
