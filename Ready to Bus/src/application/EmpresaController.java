bf291eab031a79b8a522217a68409a9ba93c80c2
João Victor Scanagatta <joaoscanagatta1@hotmail.com>
2017-09-27 19:08:10 -0300

    nova tela empresa

---
+++ Ready to Bus/src/application/EmpresaController.java
@@ -0,0 +1,43 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class EmpresaController {
+
+    @FXML
+    private Label lRota;
+
+    @FXML
+    private Button btnCadsatroMotorista;
+
+    @FXML
+    private Button btnCadastroRota;
+
+    @FXML
+    private Button btnCadastroPassageiro;
+
+    @FXML
+    private Label lApelidoMotorista;
+
+    @FXML
+    private Button btnCadsatroMotorista1;
+
+    @FXML
+    void cadastrarMotorista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cadastrarPassageiro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cadastrarRota(ActionEvent event) {
+
+    }
+
+}
