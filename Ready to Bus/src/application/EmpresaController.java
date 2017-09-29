68472f908b4cd761d7b8e66d50ce996e9a4bfa2c
João Victor Scanagatta <joaoscanagatta1@hotmail.com>
2017-09-29 00:18:00 -0300

    colocando esqueleto da empresa

--- Ready to Bus/src/application/EmpresaController.java
+++ Ready to Bus/src/application/EmpresaController.java
@@ -4,12 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import metodos.MetodosTelas;
 
 public class EmpresaController {
 
 	@FXML
-	private Label lRota;
+	private Label lRota1;
 
 	@FXML
 	private Button btnCadsatroMotorista;
@@ -21,10 +22,37 @@ public class EmpresaController {
 	private Button btnCadastroPassageiro;
 
 	@FXML
-	private Label lApelidoMotorista;
+	private Label lApelidoMotorista1;
 
 	@FXML
-	private Button btnCadsatroMotorista1;
+	private Button btnVerLista1;
+
+	@FXML
+	private ImageView imgDirigindo1;
+
+	@FXML
+	private Label lRota2;
+
+	@FXML
+	private Label lApelidoMotorista2;
+
+	@FXML
+	private Button btnVerLista2;
+
+	@FXML
+	private ImageView imgDirigindo2;
+
+	@FXML
+	private Label lRota3;
+
+	@FXML
+	private Label lApelidoMotorista3;
+
+	@FXML
+	private Button btnVerLista3;
+
+	@FXML
+	private ImageView imgDirigindo3;
 
 	private MetodosTelas tela = new MetodosTelas();
 
@@ -48,6 +76,16 @@ public class EmpresaController {
 
 	@FXML
 	void verLista1(ActionEvent event) {
+
+	}
+
+	@FXML
+	void verLista2(ActionEvent event) {
+
+	}
+
+	@FXML
+	void verLista3(ActionEvent event) {
 
 	}
 
