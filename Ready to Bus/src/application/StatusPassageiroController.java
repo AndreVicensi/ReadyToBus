ce481aeba09d5aaf3a9174b0c9a16fd3177aeae1
João Victor Scanagatta <joaoscanagatta1@hotmail.com>
2017-09-29 21:54:37 -0300

    tela passageiro

---
+++ Ready to Bus/src/application/StatusPassageiroController.java
@@ -0,0 +1,42 @@
+
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class StatusPassageiroController {
+
+	@FXML
+	private Button btnVaiVolta;
+
+	@FXML
+	private Button btnNaoVai;
+
+	@FXML
+	private Button btnSoVai;
+
+	@FXML
+	private Button btnSoVolta;
+
+	@FXML
+	void naoVai(ActionEvent event) {
+
+	}
+
+	@FXML
+	void soVai(ActionEvent event) {
+
+	}
+
+	@FXML
+	void soVolta(ActionEvent event) {
+
+	}
+
+	@FXML
+	void vaiVolta(ActionEvent event) {
+
+	}
+
+}
